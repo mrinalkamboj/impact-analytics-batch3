ba73eef14b16e550e9f952633d53d16d789757b3
Jiten <jpalaparthi@gmail.com>
2025-12-02 11:41:17 +0530

    add more examples on pointers

---
+++ 6-raw-pointers/src/main.rs
@@ -0,0 +1,66 @@
+fn main() {
+    // rust way of doing things
+    let mut x = 100;
+
+    let y = &mut x;
+
+    *y = 200;
+
+    // respecting rust borrow checker and having memory safety
+
+    let mut x = 100;
+
+    let p1: *const i32 = &x;
+
+    unsafe {
+        println!("{}", *p1);
+    }
+
+    let mut x = 100;
+
+    let y1 = &mut x; // rust managed mutable reference
+
+    // let y2 = &mut x;
+
+    *y1 = 200;
+
+    let mut x = 100;
+
+    let y1: *mut i32 = &mut x; // raw mutable pointer
+
+    let y2: *mut i32 = &mut x; // raw mutable pointer
+
+    let y3: *mut bool = &mut true;
+
+    let y4: *const i32 = std::ptr::null(); // only to the raw pointers
+
+    unsafe {
+        *y1 = 200;
+
+        *y2 = 300;
+
+        *y3 = false;
+
+        println!("{}", *y4);
+    }
+
+    println!("{}", x);
+}
+
+// rust references
+// &T
+// &mut T
+
+// raw pointers
+// * const i32 --> immutable
+// * mut i32; --> mutable
+
+// create fun --> take two integer references , give me the addition two values return a pointer
+
+/*
+
+    fn add_refs(a:&i32,b:&i32)->&i32{
+
+    }
+
+*/
